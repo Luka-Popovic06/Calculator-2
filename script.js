9d35314faa377c0148d9c3784ce796b04be4c99a
Luka Popovic <lukapopovic0111@gmail.com>
2025-06-20 23:19:51 +0200

    Adding variables in JS

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 'use strict';
+const buttons = document.querySelector('.buttons');
+const outputSecondary = document.querySelector('.output-secondary');
+const outputMain = document.querySelector('.output-main');
+let numberOne;
+let numberTwo;
+let operation;
+let finaleNumber;
