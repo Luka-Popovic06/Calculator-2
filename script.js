6716c7c241b505cdd9873afa63c976e5bbdf5963
Luka Popovic <lukapopovic0111@gmail.com>
2025-06-21 22:50:35 +0200

    Adding values of number one and number two in JS

--- script.js
+++ script.js
@@ -6,3 +6,21 @@ let numberOne;
 let numberTwo;
 let operation;
 let finaleNumber;
+let choosingNumberOne = false;
+buttons.addEventListener('click', function (e) {
+  if (e.target.closest('.btn-number')) {
+    if (choosingNumberOne === false) {
+      numberOne = e.target.value;
+      choosingNumberOne = true;
+    } else if (choosingNumberOne === true) {
+      numberTwo = e.target.value;
+      choosingNumberOne = false;
+    }
+  } else if (e.target.closest('.btn-operation')) {
+    operation = e.target.value;
+  } else if (e.target.closest('.btn-equal')) {
+    calculation(Number(numberOne), operation, Number(numberTwo));
+    outputMain.textContent = Number(finaleNumber);
+    console.log(Number(finaleNumber));
+  }
+});
