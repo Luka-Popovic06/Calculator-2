0fd5ea173e97594727b346f17a7e9f9cafed1f42
Luka Popovic <lukapopovic0111@gmail.com>
2025-06-23 22:59:09 +0200

    Output logic

--- script.js
+++ script.js
@@ -7,23 +7,35 @@ let numberTwo;
 let operation;
 let finaleNumber;
 let choosingNumberOne = false;
+let secondaryOperation = '';
+let logikaIzvrsena = false;
 buttons.addEventListener('click', function (e) {
   if (e.target.closest('.btn-number')) {
-    if (choosingNumberOne === false) {
-      numberOne = e.target.value;
-      choosingNumberOne = true;
-    } else if (choosingNumberOne === true) {
-      numberTwo = e.target.value;
-      choosingNumberOne = false;
-    }
+    biranjeBroaj(e.target.value);
   } else if (e.target.closest('.btn-operation')) {
     operation = e.target.value;
+
+    if (numberOne && operation) {
+      outputLogic();
+    }
   } else if (e.target.closest('.btn-equal')) {
     calculation(Number(numberOne), operation, Number(numberTwo));
     outputMain.textContent = Number(finaleNumber);
-    console.log(Number(finaleNumber));
+    outputSecondary.textContent = '';
   }
 });
+function outputLogic() {
+  if (logikaIzvrsena === false) {
+    outputMain.textContent = numberTwo;
+    outputSecondary.textContent = `${numberOne} ${operation}`;
+    logikaIzvrsena = true;
+    secondaryOperation = operation;
+    choosingNumberOne = true;
+  } else if (finaleNumber) {
+    calculation(Number(finaleNumber), secondaryOperation, Number(numberTwo));
+    outputSecondary.textContent = `${finaleNumber} ${secondaryOperation}`;
+  }
+}
 function calculation(a, operatio, b) {
   if (operatio === '+') {
     finaleNumber = a + b;
@@ -35,3 +47,23 @@ function calculation(a, operatio, b) {
     finaleNumber = a / b;
   }
 }
+
+function biranjeBroaj(e) {
+  if (choosingNumberOne === false) {
+    numberOne = e;
+    choosingNumberOne = true;
+    outputMain.textContent = numberOne;
+  } else if (choosingNumberOne === true) {
+    numberTwo = e;
+    choosingNumberOne = false;
+    outputMain.textContent = numberTwo;
+  }
+}
+/*if (numberOne && numberTwo && operation) {
+  // Izračunaj prethodnu operaciju
+  calculation(Number(numberOne), operation, Number(numberTwo));
+  //numberOne = String(finaleNumber);
+  numberTwo = '';
+  outputMain.textContent = '';
+  outputSecondary.textContent = `${finaleNumber} ${e.target.value}`;
+}*/
